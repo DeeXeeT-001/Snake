d5abed3665c0c99b4764e626efefcd0a433fbe2f
DeeXeeT-001 <deekshit.bti@gmail.com>
2020-12-19 19:50:44 +0545

    Added basic functionalities

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-class cube(object):
-    pass
+import pygame
+
+# Initialize Pygame
+pygame.init()
+
+width = 600
+screen = pygame.display.set_mode((width, width))
+
+# Setting the caption
+pygame.display.set_caption("Snake Game")
+# icon = pygame.image.load('./Blegh')
+
+
+def mainLoop():
+    global screen
+
+    running = True
+    screen.fill((0, 0, 0))
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.display.update()
+
+
+mainLoop()
