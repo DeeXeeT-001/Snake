6595fc4a40ed290bc36cad44322609e8b4c399ae
DeeXeeT-001 <deekshit.bti@gmail.com>
2020-12-23 21:08:54 +0545

    Snake movements on the way

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 import tkinter as tk
 from tkinter import messagebox
 from pygame import surface
-
+from pygame.constants import K_DOWN, K_LEFT, K_RIGHT, K_UP
 from pygame.time import Clock
 
 # Initialize Pygame
@@ -19,17 +19,72 @@ pygame.display.set_caption("Snake")
 # icon = pygame.image.load('./Blegh')
 
 
+class cube:
+    pass
+
+
+class snake:
+    body = []
+    turns = {}
+
+    def __init__(self, color, pos):
+        self.color = color
+        self.head = cube(pos)
+        self.body.append(self.head)
+
+        # To move in one direction only
+        self.dirnX = 0
+        self.dirnY = 1
+
+    def move(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            keys = pygame.key.get_pressed()
+
+            for key in keys:
+                if keys[K_LEFT]:
+                    self.dirnX = -1
+                    self.dirnY = 0
+                    self.turns[self.head.pos[:]] = [self.dirnX, self.dirnY]
+
+                elif keys[K_RIGHT]:
+                    self.dirnX = 1
+                    self.dirnY = 0
+                    self.turns[self.head.pos[:]] = [self.dirnX, self.dirnY]
+
+                elif keys[K_UP]:
+                    self.dirnX = 0
+                    self.dirnY = 1
+                    self.turns[self.head.pos[:]] = [self.dirnX, self.dirnY]
+
+                elif keys[K_DOWN]:
+                    self.dirnX = -0
+                    self.dirnY = -1
+                    self.turns[self.head.pos[:]] = [self.dirnX, self.dirnY]
+
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+
+
 def drawGrid(w, row, surface):
+    # Grid size
     sizeBtwn = w // row
 
     x = 0
     y = 0
 
     for l in range(row):
+
+        # Adds line in rows and column in each iteration of loop
         x += sizeBtwn
         y += sizeBtwn
 
+        # For vertical line
         pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
+
+        # For horizontal line
         pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
 
 
@@ -44,7 +99,7 @@ def redrawScreen(surface):
 def mainLoop():
     global screen
     pygame.time.delay(100)
-    # s = snake((255, 0, 0), (10, 10))
+    s = snake((255, 0, 0), (10, 10))
     running = True
     clock = pygame.time.Clock()
 
@@ -52,10 +107,6 @@ def mainLoop():
         pygame.time.delay(50)
         clock.tick(10)
         redrawScreen(screen)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
 
 
 mainLoop()
